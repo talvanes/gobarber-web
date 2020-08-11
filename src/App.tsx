1d47bff2ef62249705c229dbc5a491ca6505b440
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-11 17:57:54 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import ToastContainer from './components/ToastContainer';
+
 import { AuthProvider } from './hooks/AuthContext';
 
 import GlobalStyles from './styles/global';
@@ -12,6 +14,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyles />
   </>
